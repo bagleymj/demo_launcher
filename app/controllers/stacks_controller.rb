7817c0cec34659529882db8383b5ee32d707edb9
Mitchell Bagley <bagleymj@gmail.com>
2016-02-18 13:53:31 -0500

    Adding Resource List to Show View

--- app/controllers/stacks_controller.rb
+++ app/controllers/stacks_controller.rb
@@ -11,11 +11,9 @@ class StacksController < ApplicationController
 
   def create
     @stack = Stack.new(stack_params)
-    access_key_id = Account.all[0].access_key_id
-    secret_access_key = Account.all[0].secret_access_key
     stack_name = @stack.stack_name
     template_url = Account.all[0].template_url
-    cloudformation = Aws::CloudFormation::Client.new(access_key_id: access_key_id, secret_access_key: secret_access_key)
+    cloudformation = new_client
     new_stack = cloudformation.create_stack(stack_name: stack_name, template_url: template_url)
     if new_stack.empty?
       render :new
@@ -30,7 +28,11 @@ class StacksController < ApplicationController
   end
 
   def show
-
+    @stack = Stack.find(params[:id])
+    stack_name = @stack.stack_name
+    cloudformation = new_client
+    resp = cloudformation.list_stack_resources(stack_name: stack_name)
+    @resources = resp[0]
   end
 
 
@@ -50,12 +52,25 @@ class StacksController < ApplicationController
 
 
   def destroy
-
+    @stack = Stack.find(params[:id])
+    stack_name = @stack.stack_name
+    cloudformation = new_client
+    cloudformation.delete_stack(stack_name: stack_name)
+    @stack.destroy
+    redirect_to stacks_path
   end
 
 
 
   def stack_params
     params.require(:stack).permit(:stack_name)
+  end
+  
+  def new_client
+    access_key_id = Account.all[0].access_key_id
+    secret_access_key = Account.all[0].secret_access_key
+    cloudformation = Aws::CloudFormation::Client.new(access_key_id: access_key_id,
+                                                      secret_access_key: secret_access_key)
+    return cloudformation
   end
 end
