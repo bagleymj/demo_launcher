951d9fccf48dd9b0ff5ddab17b510f237576fdfc
Mitchell Bagley <bagleymj@gmail.com>
2016-02-24 10:11:42 -0500

    Got modals working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,19 @@
 class User < ActiveRecord::Base
+  REGEX = /A\A[a-zA-Z]\w+\z/
+  validates :company_name, 
+    presence: true,
+    format: {:with => REGEX}
+
+  validates :username,
+    presence: true,
+    uniqueness: true,
+    format: {:with => REGEX},
+    length: {:minimum => 5, :maximum => 15}
+
+  validates :display_name,
+    presence: true,
+    length: {:maximum => 50}
+
   has_secure_password
   
   has_many :stacks
