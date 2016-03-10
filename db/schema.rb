67c903770bd4662ed06639860ab5f00c22850048
Mitchell Bagley <bagleymj@gmail.com>
2016-03-10 14:08:37 -0500

    Added template model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160310171405) do
+ActiveRecord::Schema.define(version: 20160310185758) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "access_key_id"
@@ -29,6 +29,16 @@ ActiveRecord::Schema.define(version: 20160310171405) do
     t.datetime "updated_at", null: false
     t.integer  "user_id"
   end
+
+  create_table "templates", force: :cascade do |t|
+    t.string   "template_name"
+    t.string   "template_url"
+    t.integer  "account_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "templates", ["account_id"], name: "index_templates_on_account_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
