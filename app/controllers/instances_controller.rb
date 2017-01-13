4ed04c0fc9a01ee909e4609646c88adc5474f8a3
Mitchell Bagley <bagleymj@gmail.com>
2017-01-13 16:31:39 -0500

    Working on adding new instances

--- app/controllers/instances_controller.rb
+++ app/controllers/instances_controller.rb
@@ -3,6 +3,33 @@ class InstancesController < ApplicationController
     @instances = Instance.all
   end
 
+  def new
+    stack = Stack.find(params[:stack_id])
+    @instance = Instance.new
+    @title = "Add Instance to #{stack.stack_name}"
+    company_name = stack.company.company_name
+    ec2 = Account.ec2_client
+    resp = ec2.describe_instances(filters:[{name: "tag:Company", 
+                                           values:[company_name]}])
+    reservations = resp[0]
+    instance_names = []
+    reservations.each do |reservation|
+      reservation.instances.each do |instance|
+        tags = instance.tags
+        instance_name = tags.select {|tag| tag.key == 'Name'}[0].value
+        instance_names << instance_name
+      end
+    end
+    @instance_names = instance_names
+
+
+
+  end
+
+  def create
+
+  end
+
   def stop_instance
     ec2 = Account.ec2_client
     instance = Instance.find(params[:id])
