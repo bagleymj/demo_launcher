60f93402b866ebfbf55ecc9bbd53ca5f7242b7c1
Mitchell Bagley <bagleymj@gmail.com>
2017-01-17 11:38:04 -0500

    Instances now save with boot order assignment

--- app/models/instance.rb
+++ app/models/instance.rb
@@ -1,3 +1,20 @@
 class Instance < ActiveRecord::Base
+# validates :instance_id,
+#   presence: true,
+#   uniqueness: true
+# validates :boot_order,
+#   uniqueness: { 
+#     scope: :stack_id
+#   }
   belongs_to :stack
+
+  before_save :set_defaults
+
+  def set_defaults
+    stack = Stack.find(self.stack_id)
+    puts "Stack ID is #{self.stack_id}"
+    self.boot_order = stack.instances.length + 1
+    self.delay = 0
+  end
+
 end
