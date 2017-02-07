3b62ba1a0261e8755ae1289a6c5bda3d2271c400
Mitchell Bagley <bagleymj@gmail.com>
2017-02-07 10:51:08 -0500

    Added passenger to Gemfile

--- Capfile
+++ Capfile
@@ -36,7 +36,7 @@ require "capistrano/rails"
 require "capistrano/passenger"
 
 set :rvm_type, :user
-set :rvm_ruby_version, '2.4.0'
+set :rvm_ruby_version, '2.3.1'
 
 # Load custom tasks from `lib/capistrano/tasks` if you have any defined
 Dir.glob("lib/capistrano/tasks/*.rake").each { |r| import r }
--- config/deploy.rb
+++ config/deploy.rb
@@ -34,5 +34,5 @@ set :repo_url, "git@github.com:bagleymj/demo_launcher.git"
 
 set :deploy_to, '/home/deploy/stackman'
 
-append :linked_files, "config/database.yml", "config.secrets.yml"
+append :linked_files, "config/database.yml", "config/secrets.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
